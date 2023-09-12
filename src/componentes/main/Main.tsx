d04c89ef8eab0ee191b702e9c74f98fe8e9bbd7e
ThainaMedeiros06 <thaina.santos4@estudante.ifms.edu.br>
2023-09-12 10:14:09 -0400

    aula 12/09

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,13 +1,51 @@
+import { useState } from 'react'
 import Filme from './../filme/Filme'
 import './Main.css'
+type FilmeType = {
+       id:number,
+       titulo:string,
+       sinopse:string,
+       imagem:string
+}
 
 export default function Main(){
+       const [texto,setTexto]=useState("")
+
+       const filmes:FilmeType[] = [
+              {
+                     id:1,
+                     titulo:'Barbie',
+                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                     imagem:'/barbie.png'
+              },
+              {
+                     id:2,  
+                     titulo:'Barbie',
+                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                     imagem:'/KEN.png'
+              },
+              {
+                     id:3,
+                     titulo:'Barbie',
+                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
+                     imagem:'/boneca-linda-garota-gerada-por-ai.jpg'
+              }
+       ]
+
+       function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
+              setTexto(e.target.value)
+       }
     return(
     <>
         <div className='campo-pesquisa'>
-            <input type="text" className='input' placeholder='O que deseja'/>
-            <p>Digitado:</p>
+              <p>Busque um filme</p>
+            <input type="text" className='input' placeholder='Pesquise um Filme' onChange={TrataTexto}/>
+            <img className='img-lupa' src="lupa.png" alt="" />
+            { 
+               (texto)?<p>Resultados Para: {texto}</p>:"" 
+            }
         </div>
+        {/** use algo do vetor para tentar criar os filmes */}
         <main className="content-main">
             <Filme titulo='Barbie'
                    sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
