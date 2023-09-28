3b83c40b83957a99e34a30bbc50ca34aa16d07d3
ThainaMedeiros06 <thaina.santos4@estudante.ifms.edu.br>
2023-09-28 08:26:38 -0400

    linkNav 28/09/23

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import "./main.css"
+import Header from './componentes/header/Header'
+import Main from './componentes/main/Main'
 
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+              <Header/>
+              <Main/>
+            </>,
+  },
+  {
+    path: "/Home",
+    element: <div>Página Home!</div>,
+  },
+  {
+    path: "/Notícia",
+    element: <div>Página de Noticía!</div>,
+  },
+  {
+    path: "/Sobre",
+    element: <div>Página Sobre!</div>,
+  },
+  {
+    path: "/Contato",
+    element: <div>Página de Contato!</div>,
+  },
+]);
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
