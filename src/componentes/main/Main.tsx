da5878e2e78aa6398c35d7d0005417518f6a93e7
ThainaMedeiros06 <thaina.santos4@estudante.ifms.edu.br>
2023-11-24 10:55:53 -0400

    atividade finalizada

--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,81 +1,58 @@
-import axios from 'axios'
-import { useState } from 'react'
+import axios from "axios";
+import { useState, useEffect } from 'react';
 import Filme from './../filme/Filme'
 import './Main.css'
+
 type FilmeType = {
-       id:number,
-       titulo:string,
-       sinopse:string,
-       imagem:string
+  id: number,
+  titulo: string,
+  descricao: string,
+  foto: string
 }
 
-export default function Main(){
-       const [texto,setTexto]=useState("")
+export default function Main() {
+  const [texto, setTexto] = useState("");
+  const [dados, setDados] = useState<FilmeType[]>([]);
 
-       const filmes:FilmeType[] = [
-              {
-                     id:1,
-                     titulo:'Barbie',
-                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
-                     imagem:'/barbie.png'
-              },
-              {
-                     id:2,  
-                     titulo:'ken',
-                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
-                     imagem:'/KEN.png'
-              },
-              {
-                     id:3,
-                     titulo:'Barbie',
-                     sinopse:'Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.',
-                     imagem:'/boneca-linda-garota-gerada-por-ai.jpg'
-              }
-       ]
+  useEffect(() => {
+    const buscarFilmes = async () => {
+      try {
+        // Usando axios para buscar dados do backend 
+        const resposta = await axios.get('http://localhost:3000/filmes'); // Adicionei o protocolo http
+        // Atualizando o estado com os dados recebidos
+        setDados(resposta.data);
+      } catch (error) {
+        console.error("Erro ao buscar filmes:", error);
+      }
+    }
 
-       function TrataTexto(e:React.ChangeEvent<HTMLInputElement>){
-              setTexto(e.target.value)
-       }
-    return(
+    buscarFilmes(); // Chamada correta da função
+  }, []); // Adicionei os colchetes vazios para garantir que o useEffect seja executado apenas uma vez
+
+  function trataTexto(e: React.ChangeEvent<HTMLInputElement>) {
+    setTexto(e.target.value)
+  }
+
+  return (
     <>
-        <div className='campo-pesquisa'>
-              <p>Busque um filme</p>
-            <input type="text" className='input' placeholder='Pesquise um Filme' onChange={TrataTexto}/>
-            <img className='img-lupa' src="lupa.png" alt="" />
-            { 
-               (texto)?<p>Resultados Para: {texto}</p>:"" 
-            }
-        </div>
-        {/** use algo do vetor para tentar criar os filmes */}
+      <div className="pesquisar">
+        <input type="text" className='botao-pesquisa' placeholder='Pesquise um filme' onChange={trataTexto} />
+        {texto && <p>Resultados para: {texto}</p>}
+      </div>
+
+      <main className="content-main">
         {
-              filmes.filter((filme)=>filme.titulo.toLowerCase().includes(texto)).map((filme)=><Filme key={filme.id} sinopse={filme.sinopse} titulo={filme.titulo} imagem={filme.imagem}/>)
+          dados.filter((filme) => filme.titulo.toLowerCase().includes(texto)).map(
+            (filme) =>
+              <Filme
+                key={filme.id}
+                descricao={filme.descricao}
+                titulo={filme.titulo}
+                foto={filme.foto}
+              />
+          )
         }
-        <main className="content-main">
-            {/*<Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/boneca-linda-garota-gerada-por-ai.jpg'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/KEN.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>
-            <Filme titulo='Barbie'
-                   sinopse='Depois de ser expulsa da Barbieland por ser uma boneca de aparência menos do que perfeita, Barbie parte para o mundo humano em busca da verdadeira felicidade.'
-                   imagem='/barbie.png'/>*/}
-        </main>
-        </>
-    )
+      </main>
+    </>
+  )
 }
